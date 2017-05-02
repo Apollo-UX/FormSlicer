1d63f45392000e906cb510dc9279d20a84916038
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-05-02 20:46:16 -0300

    Arrumado botão disabled

--- js/script.js
+++ js/script.js
@@ -1,34 +1,31 @@
-function prox() {
-    var fs = document.getElementsByClassName('ativo');
-    var fs_atual = fs[0];
-    var fs_prox = fs_atual.nextElementSibling;
-    var btn = document.getElementsByClassName('btn-ativo');
+function prox() {                                                                   
+    var fs = document.getElementsByClassName('ativo');                      //O primeiro fieldset do index vem com a classe ativo
+    var fs_atual = fs[0];                                                   //Esse fieldset sera considerado o primeiro 
+    var fs_prox = fs_atual.nextElementSibling;                              //o prox sera o primeiro fieldset após o atual
+    var btn = document.getElementsByClassName('btn-ativo');                 //Escolhendo o botão ativo
     var btn_atual = btn[0];
-    var btn_prox = btn.nextElementSibling;
+    var btn_prox = btn[1];
 
+    
+    //Se o fieldset atual estiver aparecendo, se ele tiver um irmão existente e se o botão nao estiver desativado
     if (fs_atual.style.display !== 'none' && fs_atual.nextElementSibling !== null && !btn_atual.classList.contains("disabled")) {
-        fs_atual.style.display = 'none';
-        fs_prox.style.display = 'block';
-        fs_atual.setAttribute("class", "nativo");
+        fs_atual.style.display = 'none';                                    //Desaparece o atual
+        fs_prox.style.display = 'block';                                    //Aparece o próximo
+        fs_atual.setAttribute("class", "nativo");                           //Substitui a classe ativo por nativo e coloca ativo no proximo fieldset
         fs_prox.setAttribute("class","ativo");
         btn_atual.classList.remove("btn-ativo");
         btn_prox.classList.add("btn-ativo");
     }
 }
 
-function validar(a) {
+function validar(a) {                                                       //Função para validar se o input foi escrito corretamente
     var x = 0;
-    var teste = document.querySelectorAll(".ativo input").value; 
+    var teste = document.querySelector(".ativo input"); 
 
-    for (var index = 0; index < teste.length; index++) {
-        if (teste == "") {
-            x++;
-        }
-    }
-
-    if (x == 0) {
+    if (teste.value != "") {
         var botao = document.getElementById("button");
         botao.classList.remove("disabled");
         botao.classList.add("enable")
     }
 }
+
