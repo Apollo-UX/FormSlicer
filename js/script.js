b70d3fc940a05a36e55637b37ab59afd601d5122
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-05-03 00:38:37 -0300

    Main com automaticamente esconder os fieldsets e criar os botoes
    Signed-off-by: Gabriel Craveiro <gabriel.c.craveiro@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,31 +1,64 @@
-function prox() {                                                                   
-    var fs = document.getElementsByClassName('ativo');                      //O primeiro fieldset do index vem com a classe ativo
-    var fs_atual = fs[0];                                                   //Esse fieldset sera considerado o primeiro 
-    var fs_prox = fs_atual.nextElementSibling;                              //o prox sera o primeiro fieldset após o atual
-    var btn = document.getElementsByClassName('btn-ativo');                 //Escolhendo o botão ativo
-    var btn_atual = btn[0];
-    var btn_prox = btn[1];
+/**
+ * Ao clicar no botão "Começar processo" esconde todas as fieldsets que não sejam o primeiro
+ * Atenção para o queryselector que escolhe todos que não sejam o primeiro do tipo escolhido seria algo como (!first-fieldset)
+ */
 
-    
-    //Se o fieldset atual estiver aparecendo, se ele tiver um irmão existente e se o botão nao estiver desativado
-    if (fs_atual.style.display !== 'none' && fs_atual.nextElementSibling !== null && !btn_atual.classList.contains("disabled")) {
-        fs_atual.style.display = 'none';                                    //Desaparece o atual
-        fs_prox.style.display = 'block';                                    //Aparece o próximo
-        fs_atual.setAttribute("class", "nativo");                           //Substitui a classe ativo por nativo e coloca ativo no proximo fieldset
-        fs_prox.setAttribute("class","ativo");
-        btn_atual.classList.remove("btn-ativo");
-        btn_prox.classList.add("btn-ativo");
+function main() {
+    document.getElementById("btn-start").addEventListener("click", function () {
+        var fieldsets = document.querySelectorAll("fieldset:not(:first-of-type)");
+        var fieldsetsArr = Array.prototype.slice.call(fieldsets);
+        fieldsetsArr
+            .forEach(function (x) {
+                x.style.display = "none";
+            })
+        criarBotoes();
+    });
+
+    // var fieldsets2 = document.getElementsByClassName("next");
+    // fieldsets2.addEventListener("click", function () {
+    //     var fieldset = document.querySelectorAll("fieldset");
+    //     for (var i in fieldset) {
+    //         if (fieldset[i].style.display !== none) {
+    //             fieldset[i].style.display = none;
+    //             fieldset[i].nextElementSibling.style.display = block;
+    //             return;
+    //         }
+    //     }
+    // });
+}
+
+
+/**
+ * Criar uma coleção com todas as fieldsets
+ * No loop ele cria todas as vezes os botões, add as classes next e bgt (classe mestre)
+ * 
+ * No primeiro if ele ve se o elemento possui um outro fieldset na frente ou atrás (é um fieldset sem ser o primeiro ou o ultimo)
+ * No segundo ele ve se ele é o primeiro (possui next) poderia ser também colocado !nextElement 
+ * No ultimo ele ve se ele é o ultimo (possui prev)
+ */
+
+function criarBotoes() {
+    var fieldsets = document.getElementsByTagName("fieldset");
+    for (var i in fieldsets) {
+        var prev = document.createElement("button");
+        prev.classList.add("next", "bgt");
+        prev.innerHTML = "voltar"
+        var next = document.createElement("button");
+        next.classList.add("next", "bgt");
+        next.innerHTML = "avançar"
+
+        if (fieldsets[i].previousElementSibling && fieldsets[i].nextElementSibling) {
+            fieldsets[i].appendChild(prev);
+            fieldsets[i].appendChild(next);
+        } else if (fieldsets[i].nextElementSibling) {
+            fieldsets[i].appendChild(next);
+        } else if (fieldsets[i].previousElementSibling) {
+            fieldsets[i].appendChild(prev);
+        }
     }
 }
 
-function validar(a) {                                                       //Função para validar se o input foi escrito corretamente
-    var x = 0;
-    var teste = document.querySelector(".ativo input"); 
 
-    if (teste.value != "") {
-        var botao = document.getElementById("button");
-        botao.classList.remove("disabled");
-        botao.classList.add("enable")
-    }
-}
 
+/** Prepara função ao ativar tela */
+window.onload = main;
