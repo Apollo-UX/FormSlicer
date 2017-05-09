a1bb311c3f7cd00423884265f63b545e1faf5cca
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-05-09 18:47:27 +0000

    Editado comentários, inserido forEach e código formatado

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 /** not:
  * 
- * Partes: 
- * 
- * 1. Ao clicar no botão "Começar processo" esconde todas as fieldsets que não sejam o primeiro,
- * atenção para o seletor css que utilizei que escolhe todos que não sejam o primeiro do tipo
- * escolhido seria algo como (!firstFieldset).
- * 
- * 2. Eventlistener para o next e previous, tive que colocar no if 
- * pq elementos dinamicos criados com DOM não são automaticamente 
- * add ao event listener. Ele então reconhece se o target tem a classe desejada.
+ * Esconde todos os fieldsets que não sejam o primeiro,
+ * Eventlistener para o next e previous.
  */
 
 function main() {
     //1.
     document.getElementById("btn-start").addEventListener("click", function () {
+        // escolhe todos que não sejam o primeiro do tipo
         var fieldsets = document.querySelectorAll("fieldset:not(:first-of-type)");
         var fieldsetsArr = Array.prototype.slice.call(fieldsets);
         fieldsetsArr
@@ -35,15 +29,15 @@ function main() {
 }
 
 /**not:
- * Vi que o plugin jqueryvalidate usava de regras de regex para validar, então tive a ideia
- * de usar essas regras dentro de objetos para o mesmo.
+ * Vi que o plugin jqueryvalidate usava de regras de regex para 
+ * validar, então tive a ideia de usar essas 
+ * regras dentro de objetos para o mesmo.
  * 
- * A regra de email peguei do proprio mozilla, ele pede aquele basico de xxxxxxxx@xxxxxxx.xxx
+ * Cria uma array com todos os inputs atuais. Para cada ele 
+ * realiza um teste de regex com o value, aumentando o contador
+ * a cada erro. 
  * 
- * Na função ele está pegando o botão atualNext para saber qual fieldset está ativo no momento,
- * após isso ele cria um objeto com todos os inputs que estão nesse mesmo fieldset, usa o slice
- * para criar uma array e faz um forEach para testar todos os inputs com suas devidas regras.
- * No momento só coloquei de texto.
+ * Retorna true caso não tenha nenhum erro
  * 
  */
 
@@ -51,7 +45,7 @@ function main() {
 function validar() {
     // Regras tiradas da página do mozilla sobre regex
     var regrasREGEX = {
-        email: /[^\s]*@[a-z0-9.-]*/i,
+        email: /[^\s]*8a-z0-9.-]*/i,
         num: /^[0-9]*$/,
         texto: /[a-zA-Z]/,
         nome: /[a-zA-Z]{5,15}/,
@@ -83,12 +77,12 @@ function validar() {
  * Se ele possuir um botão antes (prev) ele coloca atualPrev. Se não existir mais um next (else) ele coloca 
  * esse botao como atualPrev
  * 
- * No fim ele remove então as classes do botão que foi clicado.
  */
 
 function prox() {
-    //NEXT
+    // NEXT
     var botaoAtual = document.getElementsByClassName("atualNext")[0];
+    // Usa a função validar para conferir os inputs e liberar o botão
     if(!validar()){
         return;
     }
@@ -108,14 +102,15 @@ function prox() {
  * Reconhece o primeiro botão atualPrev, coloca o fieldset (parente) como display:none 
  * pega o proximo fieldset irmão do parente e coloca block.
  * 
- * No if ele pega o pai, depois pega o ultimo elemento igual ao pai antes dele, procura o ultimo filho, pega o ultimo irmão e ve se é next ou previous
- * a confusão maior é pra manter o atualNext e na hora que acaba os previous, mas isso é resolvido no else (que ve se ele possui um irmao pro ultimo filho).
+ * No if ele pega o pai, depois pega o ultimo elemento igual ao pai antes dele, 
+ * procura o ultimo filho, pega o ultimo irmão e ve se é next ou previous
+ * a confusão maior é pra manter o atualNext e na hora que acaba os previous, 
+ * mas isso é resolvido no else (que ve se ele possui um irmao pro ultimo filho).
  * 
- * No fim ele remove então as classes do botão que foi clicado.
  */
 
 function prev() {
-    //PREVIOUS
+    // Previous
     var botaoAtual = document.getElementsByClassName("atualPrev")[0];
     botaoAtual.parentElement.style.display = "none";
     botaoAtual.parentElement.previousElementSibling.style.display = "block";
@@ -154,16 +149,10 @@ function ultimoFilhoPreviousPai(botaoAtual) {
 
 
 /** not:
- * Antes do loop: Ele então cria uma variavel pega todos os fieldsets (collection).
- * 
- * No loop ele vai então começar a criar os botões de previous e next para inserir nos fieldsets. Eu inseri a classe bgt para então depois
- * criarmos o nosso css padrão da biblioteca, não usei innerHTML pois pode causar altos erros depois, por fim eu coloquei .type = "button"
- * pois o botão dentro de uma form tem a propriedade padrão de dar refresh na pagina, causando então que tudo desmoronasse :(.
- * 
- * 
- * No primeiro if ele ve se o elemento possui um outro fieldset na frente ou atrás (é um fieldset sem ser o primeiro ou o ultimo),
- * no segundo ele ve se ele é o primeiro (possui next) poderia ser também colocado !nextElement é também inserida a classe atualnext,
- * no ultimo ele ve se ele é o ultimo (possui prev).
+ * Pega as arrays e transforma em uma Array, depois realiza
+ * a criação dos botões next e previous com as devidas classes
+ * o primeiro botão next vem com a classe atualNext para
+ * depois possibilitar a navegação
  * 
  */
 
@@ -174,14 +163,14 @@ function criarBotoes() {
 }
 function pegaFs(v){
     v.forEach(function(x){
-       var prev = document.createElement("button");
-       prev.classList.add("prev", "bgt");
-       prev.textContent = "voltar"
-       prev.type = "button";
-       var next = document.createElement("button");
-       next.classList.add("next", "bgt");
-       next.textContent = "avançar"
-       next.type = "button";
+       var prev = document.createElement("button")
+           .classList.add("prev", "bgt")
+           .textContent = "voltar"
+           .type = "button";
+       var next = document.createElement("button")
+           .classList.add("next", "bgt")
+           .textContent = "avançar"
+           .type = "button";
        
        if (x.previousElementSibling && x.nextElementSibling) {
             x.appendChild(prev);
